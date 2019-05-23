bd03bd8fffdc07dc6f3cfa4bb2b6c55c4c1fc29a
tianxzhang <tianxzhang@163.com>
2019-05-23 03:39:02 +0000

    Updated database configuration

---
+++ bootstrap/helpers.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @Author: tianxzhang
+ * @Date:   2019-05-23 11:29:56
+ * @Last Modified by:   tianxzhang
+ * @Last Modified time: 2019-05-23 11:33:03
+ */
+
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
